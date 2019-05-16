884c62bb9e945fc82b159898b8fb743921f7c949
brimers <brimers@sheridancollege.ca>
2019-05-16 15:19:32 -0400

    localStorage test

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,2 +1,24 @@
 // localStorage.js
 /* Later we will be using Classes and loading student data from external data file */
+
+
+function saveData(){
+    //alert('hi');
+    let sname1 = document.getElementById('sname1').value;
+    let sid1 = document.getElementById('sid1').value;
+    let fees1 = document.getElementById('fees1').value;
+    let scholarship1 = document.getElementById('scholarship1').value;
+    let tuition1 = document.getElementById('tuition1').value;
+
+    localStorage.setItem('sname1', sname1);
+    localStorage.setItem('sid1', sid1);
+    localStorage.setItem('fees1', fees1);
+    localStorage.setItem('scholarship1', scholarship1);
+    localStorage.setItem('tuition1', tuition1);
+}
+
+function makeReport(){
+    //alert('hi2');
+
+
+}
